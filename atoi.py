160ad4aa425a6b8ce6a7782527b4a11b4cd583df
Jesse White <jsperlingwhite@gmail.com>
2017-03-29 23:13:01 -0700

    Update atoi.py

--- atoi.py
+++ atoi.py
@@ -1,3 +1,8 @@
+'''
+Problem: Implement atoi (a function in C/++). Do all research yourself. Deliberately vague.
+Reference: C++ documentation for atoi function.
+I missed uncommonly many test cases for this one (although it's done now). I'll comment them ALL in.
+'''
 class Solution(object):
     import string
     def myAtoi(self, st):
@@ -6,7 +11,11 @@ class Solution(object):
         :rtype: int
         """
         
-        s = ''.join(st.strip())
+        # CASE: used to split(' '), which is wrong -- internal whitespace should throw an exception
+        # n.b. i used to have the join() method here, but it's not necessary w/out split().
+        # oh speaking of which, CASE, I also didn't remember that string.join() returns a new string,
+        # rather than replacing the original string
+        s = st.strip()
         
         result = 0
         negative = False
@@ -14,12 +23,14 @@ class Solution(object):
         if len(s)>=2:
             if s[0]=='+':
                 s = s[1:]
+            # CASE: initially had another if here. they hit me with something like "+-6". that one was clever
             elif s[0]=='-':
                 negative = True
                 s = s[1:]
         
         try:
             while s:
+                # CASE: i figured int(" ") would throw a ValueError; it doesn't. it just returns 0.
                 if s[0] in string.whitespace:
                     break
                 result = int(str(result) + s[0])
@@ -27,13 +38,11 @@ class Solution(object):
         
         except ValueError:
             pass
-            #if negative: result = -1*result
-            #return result
         
         if negative: result = -1*result
+        # CASE: the c++ documentation literally doesn't even deal with this, but i had to. i don't blame myself for this one.
         if result>2147483647:
             result = 2147483647
         elif result<-2147483648:
             result = -2147483648
         return result
-    
