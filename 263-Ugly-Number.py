f2ac57a4d988dc54458ea7fa575cb05bc8cf3509
Jesse White <jsperlingwhite@gmail.com>
2017-05-03 00:01:02 -0700

    Update 263-Ugly-Number.py
    Took out unnecessary check

--- 263-Ugly-Number.py
+++ 263-Ugly-Number.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: bool
         """
         if num<1: return False
-        if num==1: return True
         while num%2==0: num /= 2
         while num%3==0: num /= 3
         while num%5==0: num /= 5
