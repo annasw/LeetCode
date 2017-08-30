003427291c16e459eb682fc85aeb82843b29863e
Jesse White <jsperlingwhite@gmail.com>
2017-08-30 00:28:48 -0700

    Create 453-Minimum-Moves-to-Equal-Array-Elements.py

---
+++ 453-Minimum-Moves-to-Equal-Array-Elements.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minMoves(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # this is way easier to think about as decrementing one number
+        
+        minElt = min(nums)
+        return sum([n-minElt for n in nums])
+        
+        # one-liner that works, but too slowly, b/c min(nums) is O(n)
+        # return sum([n-min(nums) for n in nums])
+        
