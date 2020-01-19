59d548a2f868f95f3a280aa90084102908287062
Anna White <jsperlingwhite@gmail.com>
2020-01-19 03:32:32 -0800

    Create 50-Pow(x,n).py

---
+++ 50-Pow(x,n).py
@@ -0,0 +1,6 @@
+# this is medium difficulty
+# i guess maybe they wanted like a lower-level answer... but this isn't even that high-level it's just built into python
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
